e50967d561621d6aeed0e6980e60352c23b4b7d4
andyyy912 <48918488+andyyy912@users.noreply.github.com>
2019-05-31 19:33:35 -0300

    Agrego test uso de herramientas con nuevo funcionamiento
    Pruebo usar un pico para juntar madera. El pico debe desgastarse pero la madera no.

--- test/TestUsoHerramientas.java
+++ test/TestUsoHerramientas.java
@@ -1,7 +1,5 @@
-import Modelo.Herramientas.HachaDeMadera;
-import Modelo.Herramientas.Herramienta;
-import Modelo.Materiales.Madera;
-import Modelo.Materiales.Material;
+import Modelo.Herramientas.*;
+import Modelo.Materiales.*;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -9,11 +7,20 @@ import static org.junit.Assert.assertEquals;
 public class TestUsoHerramientas {
 
     @Test
-    public void test01UsarHachaMaderaContraMaderaReduceAmbasDurabilidades() {
+    public void test01UsarHachaContraMaderaReduceAmbasDurabilidades() {
         Material madera = new Madera();
         Herramienta hacha = new HachaDeMadera();
         hacha.usar(madera);
         assertEquals(8,madera.getDurabilidad());
         assertEquals(98,hacha.getDurabilidad(),0.0f);
+    }
+
+    @Test
+    public void test02UsarPicoContraMaderaReduceSoloDurabilidadDelPico() {
+        Material madera = new Madera();
+        Herramienta pico = new PicoDeMadera();
+        pico.usar(madera);
+        assertEquals(10,madera.getDurabilidad());
+        assertEquals(98,pico.getDurabilidad(),0.0f);
     }
 }
