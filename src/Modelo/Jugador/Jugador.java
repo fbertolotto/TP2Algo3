080d52d33c93513f70461a2d6a32c74fd363082f
andyyy912 <48918488+andyyy912@users.noreply.github.com>
2019-06-02 16:13:34 -0300

    Agrego Clase Jugador y sus test iniciales

---
+++ src/Modelo/Jugador/Jugador.java
@@ -0,0 +1,23 @@
+package Modelo.Jugador;
+
+import Modelo.Herramientas.*;
+
+import java.util.ArrayList;
+
+
+public class Jugador {
+
+    protected ArrayList inventario = new ArrayList();
+
+    public Jugador() {
+        inventario.add(new HachaDeMadera());
+    }
+
+    public boolean tieneUnaHerramienta(Class ClaseDeHerramienta) {
+        for ( int i = 0; i < inventario.size(); i++) {
+            if (inventario.get(i).getClass().isAssignableFrom(ClaseDeHerramienta)) { return true; }
+        }
+        return false;
+    }
+
+}
---
+++ test/TestJugador.java
@@ -0,0 +1,22 @@
+import Modelo.Herramientas.HachaDeMadera;
+import Modelo.Jugador.*;
+import org.junit.Test;
+
+import static junit.framework.TestCase.*;
+
+public class TestJugador {
+
+    @Test
+    public void test01ElJugadorSeCreaCorrectamente() {
+        Jugador jugador = new Jugador();
+        assertNotNull(jugador);
+    }
+
+    @Test
+    public void test02ElJugadorSeCreaConUnHacha() {
+        Jugador jugador = new Jugador();
+        assertTrue(jugador.tieneUnaHerramienta(HachaDeMadera.class));
+    }
+
+
+}
