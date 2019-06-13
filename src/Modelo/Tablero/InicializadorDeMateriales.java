e9b8f7db74ee3bbd8c3263d1357cd642f61b291e
andyyy912 <48918488+andyyy912@users.noreply.github.com>
2019-06-13 11:07:34 -0300

    Refactor sobrecarga de métodos

--- src/Modelo/Tablero/InicializadorDeMateriales.java
+++ src/Modelo/Tablero/InicializadorDeMateriales.java
@@ -11,31 +11,31 @@ public class InicializadorDeMateriales {
 	}
 
 	public void inicializarMateriales() {
-		inicializarMaterialMadera(8);
-		inicializarMaterialMetal(4);
-		inicializarMaterialPiedra(6);
-		inicializarMaterialDiamante(2);
+		inicializarMaterial(new Madera(),8);
+		inicializarMaterial(new Piedra(),6);
+		inicializarMaterial(new Metal(),4);
+		inicializarMaterial(new Diamante(),2);
 	}
 
-	private void inicializarMaterialMadera(int cant) {
+	private void inicializarMaterial(Madera unaMadera,int cant) {
 		for (int i = 0; i < cant; i++) {
 			Madera madera = new Madera();
 			posicionador.crearPosicionRandomYAgregarElemento(madera);
 		}
 	}
-	private void inicializarMaterialMetal(int cant) {
+	private void inicializarMaterial(Metal unMetal,int cant) {
 		for (int i = 0; i < cant; i++) {
 			Metal metal = new Metal();
 			posicionador.crearPosicionRandomYAgregarElemento(metal);
 		}
 	}
-	private void inicializarMaterialDiamante(int cant) {
+	private void inicializarMaterial(Diamante unDiamante,int cant) {
 		for (int i = 0; i < cant; i++) {
 			Diamante diamante = new Diamante();
 			posicionador.crearPosicionRandomYAgregarElemento(diamante);
 		}
 	}
-	private void inicializarMaterialPiedra(int cant) {
+	private void inicializarMaterial(Piedra unaPiedra,int cant) {
 		for (int i = 0; i < cant; i++) {
 			Piedra piedra = new Piedra();
 			posicionador.crearPosicionRandomYAgregarElemento(piedra);
