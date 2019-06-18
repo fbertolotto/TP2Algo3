d3e59c510934fd647cd133e1d1b9640381522dfd
andyyy912 <48918488+andyyy912@users.noreply.github.com>
2019-06-18 01:33:24 -0300

    fix refactor

---
+++ src/Controlador/BotonUsarHerramienta.java
@@ -0,0 +1,21 @@
+package Controlador;
+
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.File;
+
+public class BotonUsarHerramienta extends Button {
+
+	public BotonUsarHerramienta() {
+		File file = new File("media/images/usarHerramienta.png/");
+		Image image = new Image(file.toURI().toString());
+		ImageView imageView = new ImageView(image);
+		imageView.setFitHeight(60);
+		imageView.setFitWidth(60);
+		this.setGraphic(imageView);
+		this.setLayoutX(1700);
+		this.setLayoutY(150);
+	}
+}
